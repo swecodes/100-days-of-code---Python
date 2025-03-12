08acc42ece1bf17edc34617569f0140cf59c7692
Swetha <190694215+swecodes@users.noreply.github.com>
2025-03-12 10:48:04 +0530

    Add files via upload

---
+++ band name generator.py
@@ -0,0 +1,2 @@
+print("Welcome to the Band Name Generator.")
+print(( "Your band name could be ") + input("What's the name of the city you grew up in? \n") + input( "What is your pet's name? \n") ) 
